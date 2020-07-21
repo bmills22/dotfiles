d6661c170492049c3bb140aa557e563905597d08
Blake Miller <blake.miller714@gmail.com>
2020-07-20 18:16:37 -0700

    various theme and color tweaks

--- vimrc
+++ vimrc
@@ -1,7 +1,9 @@
 syntax on
-let g:molokai_original = 1
-colorscheme molokai
 set tabstop=4
 set softtabstop=4
 set expandtab
 set number
+
+packadd! dracula
+syntax enable
+colorscheme dracula
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,21 +1,24 @@
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Util.SpawnOnce
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.WorkspaceHistory
 
 myTerminal = "alacritty"
 myBorderWidth = 2
 myModMask = mod4Mask
 
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+myWorkspaces = ["main", "web", "code", "4", "5", "6", "7", "8", "9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myNormalBorderColor  = "#292d3e"
+myFocusedBorderColor = "#bbc5ff"
 
 myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
     where
@@ -30,20 +33,41 @@ myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
         -- Percent of screen to increment by when resizing panes
         delta = 3/100
 
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+myStartupHook :: X ()
 myStartupHook = do
     spawnOnce "nitrogen --restore &"
     spawnOnce "picom &"
+    spawnOnce "trayer --edge top --iconspacing 0 --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x292d3e --height 22 &"
+
+myLogHook :: X ()
+myLogHook = fadeInactiveLogHook fadeAmount
+    where fadeAmount = 1.0
+    
 main = do
-    xmproc <- spawnPipe "xmobar -x 0 /home/blake/.xmobarrc"
+   -- xmproc <- spawnPipe "xmobar -x 0 /home/blake/.xmobarrc0"
+    xmproc0 <- spawnPipe "xmobar -x 0 /home/blake/.xmobarrc"
+    xmproc1 <- spawnPipe "xmobar -x 1 /home/blake/.xmobarrc"
+    xmproc2 <- spawnPipe "xmobar -x 2 /home/blake/.xmobarrc"
     
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = myLayout 
         , startupHook = myStartupHook
         , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
+        , logHook = workspaceHistoryHook <+> myLogHook <+> dynamicLogWithPP xmobarPP
+                        { ppOutput = \x -> hPutStrLn xmproc0 x  >> hPutStrLn xmproc1 x  >> hPutStrLn xmproc2 x
+                        , ppCurrent = xmobarColor "#c3e88d" "" . wrap "[" "]" -- Current workspace in xmobar
+                        , ppVisible = xmobarColor "#c3e88d" ""                -- Visible but not current workspace
+                        , ppHidden = xmobarColor "#82AAFF" "" . wrap "*" ""   -- Hidden workspaces in xmobar
+                        , ppHiddenNoWindows = xmobarColor "#c792ea" ""        -- Hidden workspaces (no windows)
+                        , ppTitle = xmobarColor "#b3afc2" "" . shorten 60     -- Title of active window in xmobar
+                        , ppSep =  "<fc=#666666> <fn=2>|</fn> </fc>"                     -- Separators in xmobar
+                        , ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"  -- Urgent workspace
+                        , ppExtras  = [windowCount]                           -- # of windows current workspace
+                        , ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]
                         },
             borderWidth = myBorderWidth,
             terminal = myTerminal,
